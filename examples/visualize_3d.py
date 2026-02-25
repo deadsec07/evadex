0fa9d287727d8ff97d400b42c4bb4af23e964651
deadsec07 <deadsec07@gmail.com>
2026-02-25 18:43:46 +0530

    style: apply Black formatting per CI (pyproject line-length=100)

--- examples/visualize_3d.py
+++ examples/visualize_3d.py
@@ -33,6 +33,7 @@ class Missile3D:
         self.pos += dir_vec * self.speed
         self.traj.append(self.pos.copy())
 
+
 class Interceptor3D:
     def __init__(self, position, speed=0.8):
         self.pos = np.array(position, dtype=float)
@@ -46,28 +47,31 @@ class Interceptor3D:
             self.pos += dir_vec * self.speed
         self.traj.append(self.pos.copy())
 
+
 # Initialize scenario
 missile = Missile3D(position=[0, 0, 0], goal=[100, 0, 0])
 interceptors = [
     Interceptor3D([0, 20, 20]),
     Interceptor3D([0, -20, 20]),
-    Interceptor3D([0, 20, -20])
+    Interceptor3D([0, 20, -20]),
 ]
 
 # Set up 3D plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-mis_line, = ax.plot([], [], [], lw=2, label='Missile')
-int_lines = [ax.plot([], [], [], lw=1, label=f'Interceptor {i+1}')[0] 
-             for i in range(len(interceptors))]
+ax = fig.add_subplot(111, projection="3d")
+(mis_line,) = ax.plot([], [], [], lw=2, label="Missile")
+int_lines = [
+    ax.plot([], [], [], lw=1, label=f"Interceptor {i+1}")[0] for i in range(len(interceptors))
+]
 
 ax.set_xlim(0, 110)
 ax.set_ylim(-30, 30)
 ax.set_zlim(-30, 30)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 ax.legend()
+
 
 # Initialization for animation
 def init():
@@ -78,19 +82,21 @@ def init():
         ln.set_3d_properties([])
     return [mis_line] + int_lines
 
+
 # Update function called each frame
 def update(frame):
     missile.step(interceptors)
     for ic in interceptors:
         ic.step(missile)
     traj = np.array(missile.traj)
-    mis_line.set_data(traj[:,0], traj[:,1])
-    mis_line.set_3d_properties(traj[:,2])
+    mis_line.set_data(traj[:, 0], traj[:, 1])
+    mis_line.set_3d_properties(traj[:, 2])
     for idx, ic in enumerate(interceptors):
         t_ic = np.array(ic.traj)
-        int_lines[idx].set_data(t_ic[:,0], t_ic[:,1])
-        int_lines[idx].set_3d_properties(t_ic[:,2])
+        int_lines[idx].set_data(t_ic[:, 0], t_ic[:, 1])
+        int_lines[idx].set_3d_properties(t_ic[:, 2])
     return [mis_line] + int_lines
+
 
 # Launch animation
 ani = FuncAnimation(fig, update, frames=200, init_func=init, interval=50, blit=False)
